3f95799b4c7fb570f56e7597233fb34f2ccd67c8
Taufiq Stark <48915546+TaufiqStark@users.noreply.github.com>
2019-04-05 21:12:52 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+
+#perulangan
+ulang='y' 
+while [ $ulang = 'y' ]; 
+do 
+   #menu utama 
+   echo '1) Install Red Hawk' 
+   echo '2) Install cmbah-v2' 
+   echo '3) Install LazyGo' 
+   echo '4) Install FbCrack' 
+   echo '5) Install HammerDDOS'
+   echo '0) Exit' 
+   #masukan atau input 
+   read -p 'Masukkan Pilhan anda : ' pil; 
+   #kondisi jika input = 1 maka akan menginstall redhawk
+   if [ $pil = '1' ] 
+   then 
+   echo 'installing Red Hawk' 
+   cd $HOME 
+   apt update && apt upgrade -y 
+   apt install php -y 
+   apt install git.
+   git clone https://github.com/Tuhinshubhra/RED_HAWK 
+   echo 'installing succes....! ' 
+   sleep 1 
+   #kondisi jika input = 2 maka akan menginstall Cmbah-v2 
+   elif [ $pil = '2' ];
+   then 
+   echo 'installing Cmbah v2 ' 
+   cd $HOME 
+   apt update && apt upgrade -y 
+   apt install python -y
+   apt install git -y 
+   git clone https://github.com/kadal15/Cmbah-v2 
+   cd Cmbah-v2 
+   pip install -r requirements.txt 
+   echo 'installing succes...! ' 
+   sleep 1 
+   #kondisi jika input = 3 maka akan menginstall LazyGo 
+   elif [ $pil = '3' ]; 
+   then 
+   echo 'installing LazyGo' 
+   cd $HOME 
+   apt update && apt upgrade -y 
+   apt install git 
+   git clone https://github.com/kadal15/LazyGo.git 
+   cd LazyGo 
+   sh install.sh
+   echo 'installing succes.. ' 
+   sleep 1 
+   #kondisi jika input = 4 maka akan menginstall fbcrack
+   if [ $pil = '4' ] 
+   then 
+   echo 'Installing FbCrack' 
+   cd $HOME 
+   apt update && apt upgrade -y 
+   apt install php -y 
+   apt install git.
+   apt install python2 -y
+   git clone https://github.com/INDOnimous/FB-Crack-.git
+   echo 'installing succes....! ' 
+   sleep 1 
+   #kondisi jika input = 4 maka akan menginstall hammer ddos
+   if [ $pil = '5' ] 
+   then 
+   echo 'Installing HammerDDOS' 
+   cd $HOME 
+   apt update && apt upgrade -y 
+   apt install php -y  
+   apt install git.
+   apt install python2 -y
+   apt install python -y
+   git clone https://github.com/cyweb/hammer
+   echo 'installing succes....! ' 
+   sleep 1 
+   #kondisi jika input = 0 maka akan keluar dari tools 
+   elif [ $pil = '0' ]; 
+   then 
+   echo 'bye bye.....! ' 
+   sleep 2 
+   exit 
+   #kondisi else 
+   else 
+   echo 'ERROR : Wrong Input....! ' 
+   sleep 1 
+   echo $ulang 
+   fi
